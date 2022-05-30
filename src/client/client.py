86418ac4fb4c988c33457e3d3af29695868cf776
Adam Bako <bakoad@gmail.com>
2022-05-30 17:48:57 +0200

    initial func version

--- src/client/client.py
+++ src/client/client.py
@@ -2,7 +2,10 @@ import hmac
 import base64
 from urllib.parse import unquote
 from keboola.http_client import HttpClient
-from requests.exceptions import HTTPError
+import urllib
+from typing import Optional
+
+from typing import Generator, Dict
 
 BASE_URL = ""
 
@@ -12,22 +15,54 @@ class K2ClientException(Exception):
 
 
 class K2Client(HttpClient):
-    def __init__(self, username: str, password: str, source_url: str):
-        self.auth = self._get_auth_header(username, password, source_url)
-        super().__init__(BASE_URL)
+    def __init__(self, username: str, password: str, k2_address: str, service_name: str) -> None:
+        self.username = username
+        self.password = password
+        self.k2_address = k2_address
+        self.service_name = service_name
+        super().__init__(f"{k2_address}/{service_name}", max_retries=3)
+
+    def get_object_meta(self, object_name: str) -> Dict:
+        requests_url = f"{self.base_url}Meta/{object_name}"
+        auth_header = self._get_auth_header(self.username, self.password, requests_url)
+        header = {"Authorization": auth_header}
+        return self.get(requests_url, is_absolute_path=True, headers=header)  # noqa returns Dict even though typehint is requests.Response
+
+    def get_object_data(self, object_name: str, fields: Optional[str], conditions: Optional[str]) -> Generator:
+        parameters = {"pageSize": 250}
+        if fields:
+            parameters["fields"] = fields
+        if conditions:
+            parameters["conditions"] = conditions
+
+        encoded_params = urllib.parse.urlencode(parameters)
+        requests_url = f"{self.base_url}Data/{object_name}?{encoded_params}"
+
+        auth_header = self._get_auth_header(self.username, self.password, requests_url)
+        header = {"Authorization": auth_header}
+        last_page = False
+        next_page_url = ""
+        while not last_page:
+            if next_page_url:
+                auth_header = self._get_auth_header(self.username, self.password, next_page_url)
+                header = {"Authorization": auth_header}
+                current_page = self.get(next_page_url, is_absolute_path=True, headers=header)
+            else:
+                current_page = self.get(requests_url, is_absolute_path=True, headers=header)
+            next_page_url = current_page.get("NextPageURL")
+            if not next_page_url:
+                last_page = True
+            yield current_page.get("Items")
 
     def _get_auth_header(self, username: str, password: str, source_url: str) -> str:
         hmac_hash = self._generate_hmac_hash(password, source_url)
         return f"{username}:{hmac_hash}"
 
     @staticmethod
-    def _generate_hmac_hash(password: str, source_url: str):
-        source_url_decoded = unquote(source_url)
-        hmac_hash = hmac.new(password.encode('utf-8'), source_url_decoded.encode('utf-8'), 'MD5')
-        return base64.b64encode(hmac_hash.digest()).decode()
-
-    def get_endpoint(self, endpoint):
-        try:
-            return self.get(endpoint_path=endpoint)
-        except HTTPError as http_err:
-            raise K2ClientException(http_err) from http_err
+    def _generate_hmac_hash(password: str, source_url: str) -> str:
+        unquoted_source_url = unquote(source_url)
+        message = unquoted_source_url.upper().encode()
+        key = password.encode()
+        hmac_hash = hmac.new(key, msg=message, digestmod='md5')
+        base_44_encoded_hash = base64.b64encode(hmac_hash.digest())
+        return base_44_encoded_hash.decode()
--- src/component.py
+++ src/component.py
@@ -2,19 +2,41 @@ import logging
 
 from keboola.component.base import ComponentBase
 from keboola.component.exceptions import UserException
+from sshtunnel import SSHTunnelForwarder
+from keboola.csvwriter import ElasticDictWriter
+from typing import List
 
 from client import K2Client, K2ClientException
 
 KEY_USERNAME = "username"
 KEY_PASSWORD = "#password"
+KEY_DATA_OBJECT = "data_object"
+KEY_FIELDS = "fields"
+KEY_CONDITIONS = "conditions"
+KEY_SOURCE_URL = "source_url"
+KEY_SERVICE_NAME = "service_name"
+
+KEY_USE_SSH = "use_ssh"
+KEY_SSH = "ssh"
+KEY_SSH_PRIVATE_KEY = "#private_key"
+KEY_SSH_USERNAME = "username"
+KEY_SSH_TUNNEL_HOST = "tunnel_host"
+KEY_SSH_REMOTE_ADDRESS = "remote_address"
+KEY_SSH_REMOTE_PORT = "remote_port"
+
+KEY_STATE_PREVIOUS_COLUMNS = "previous_columns"
 
 REQUIRED_PARAMETERS = [KEY_USERNAME, KEY_PASSWORD]
 REQUIRED_IMAGE_PARS = []
+
+LOCAL_BIND_ADDRESS = "localhost"
+LOCAL_BIND_PORT = 9800
 
 
 class Component(ComponentBase):
 
     def __init__(self):
+        self.ssh_server = None
         super().__init__()
 
     def run(self):
@@ -22,16 +44,75 @@ class Component(ComponentBase):
         self.validate_image_parameters(REQUIRED_IMAGE_PARS)
         params = self.configuration.parameters
 
+        state = self.get_state_file()
+        previous_columns = state.get(KEY_STATE_PREVIOUS_COLUMNS)
+        if not previous_columns:
+            previous_columns = []
+
         username = params.get(KEY_USERNAME)
         password = params.get(KEY_PASSWORD)
-        source_url = params.get(KEY_PASSWORD)
+        data_object = params.get(KEY_DATA_OBJECT)
+        if fields := params.get(KEY_FIELDS):
+            fields = fields.replace(" ", "")
+        conditions = params.get(KEY_CONDITIONS)
+        service_name = params.get(KEY_SERVICE_NAME)
 
-        client = K2Client(username, password, source_url)
+        if params.get(KEY_USE_SSH):
+            self.create_ssh_tunnel()
+            self.ssh_server.start()
+
+        k2_address = self.get_k2_address()
+
+        client = K2Client(username, password, k2_address, service_name)
+
+        # object_meta = client.get_object_meta(data_object)
+        table = self.create_out_table_definition(f"{data_object}.csv")
+        elastic_writer = ElasticDictWriter(table.full_path, previous_columns)
 
         try:
-            client.get_endpoint("test")
-        except K2ClientException as client_exc:
-            raise UserException(client_exc) from client_exc
+            for page_data in client.get_object_data(data_object, fields, conditions):
+                parsed_data = self.parse_object_data(page_data)
+                elastic_writer.writerows(parsed_data)
+        except K2ClientException as k2_exc:
+            raise UserException(k2_exc) from k2_exc
+        table.columns = elastic_writer.fieldnames
+        elastic_writer.close()
+        self.write_manifest(table)
+
+    @staticmethod
+    def parse_object_data(data: List) -> List:
+        parsed_data = []
+        for datum in data:
+            row = {}
+            for field_values in datum.get("FieldValues"):
+                row[field_values.get("Name")] = field_values.get("Value")
+            parsed_data.append(row)
+        return parsed_data
+
+    def create_ssh_tunnel(self) -> None:
+        params = self.configuration.parameters
+        ssh = params.get(KEY_SSH)
+        ssh_private_key = ssh.get(KEY_SSH_PRIVATE_KEY)
+        ssh_tunnel_host = ssh.get(KEY_SSH_TUNNEL_HOST)
+        ssh_remote_address = ssh.get(KEY_SSH_REMOTE_ADDRESS)
+        try:
+            ssh_remote_port = int(ssh.get(KEY_SSH_REMOTE_PORT))
+        except ValueError as v_e:
+            raise UserException("Remote port must be a valid integer") from v_e
+        ssh_username = ssh.get(KEY_SSH_USERNAME)
+
+        self.ssh_server = SSHTunnelForwarder(ssh_address_or_host=ssh_tunnel_host,
+                                             ssh_pkey=ssh_private_key,
+                                             ssh_username=ssh_username,
+                                             remote_bind_address=(ssh_remote_address, ssh_remote_port),
+                                             local_bind_address=(LOCAL_BIND_ADDRESS, LOCAL_BIND_PORT))
+
+    def get_k2_address(self) -> str:
+        params = self.configuration.parameters
+        if params.get(KEY_USE_SSH):
+            return f"http://{LOCAL_BIND_ADDRESS}:{LOCAL_BIND_PORT}"
+        source_url = params.get(KEY_SOURCE_URL)
+        return f"http://{source_url}" if "http://" not in source_url else source_url
 
 
 if __name__ == "__main__":
